d3c305f409ab8373db0a9bc31261903024bbb26a
thoran <code@thoran.com>
2015-03-19 19:59:49 +1100

    Committed automatically by git-importall.

--- lib/HTTP/post.rb
+++ lib/HTTP/post.rb
@@ -1,26 +1,24 @@
 # HTTP/post.rb
 # HTTP#post.rb
 
-# 20130307, 08, 09
-# 0.8.2
+# 20130309
+# 0.8.3
 
 # Changes: 
 # 1. The parameter, data, can now also accept a :header argument which contains a hash of key-value pairs to be set as part of the header for the request.  
 # 1/2
 # 2. /header/headers/.  
+# 2/3
+# 3. Headers are now set via a separate parameter, since it is possible that form data may include the key 'headers'!  
+# 4. - URI::HTTP.interpolated_port.  
 
-# Notes: This doesn't return a MechanizeHelper::Page as was intended by the others, but it does work...  (Will get to the MechanizeHelper::Page version later.)
+# Notes: This doesn't return a MechanizeHelper::Page as was intended by the others, but it does work...  (Will get to the MechanizeHelper::Page version later.)  
 
-require '_meta/blankQ'
 require 'net/http'
 require 'uri'
 
 module URI
   class HTTP
-
-    def interpolated_port
-      scheme == 'https' ? 443 : 80
-    end
 
     def use_ssl?
       scheme == 'https' ? true : false
@@ -31,16 +29,14 @@ end
 
 module HTTP
 
-  def post(uri, data)
-    uri_object = URI.parse(uri)
-    http = Net::HTTP.new(uri_object.host, uri_object.port || uri_object.default_port)
-    http.use_ssl = uri_object.use_ssl?
+  def post(uri, form_data = {}, headers = {})
+    uri = URI.parse(uri)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = uri.use_ssl?
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    request_object = Net::HTTP::Post.new(uri_object.request_uri)
-    if headers = data.delete(:headers) || data.delete('headers')
-      headers.each{|k,v| request_object[k] = v}
-    end
-    request_object.set_form_data(data)
+    request_object = Net::HTTP::Post.new(uri.request_uri)
+    request_object.form_data = form_data
+    headers.each{|k,v| request_object[k] = v}
     response = http.request(request_object)
     response.body
   end
