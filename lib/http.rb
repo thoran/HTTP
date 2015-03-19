c2aaab45522563301ffb3b90f4e7a4fdae79365b
thoran <code@thoran.com>
2015-03-19 19:59:49 +1100

    Committed automatically by git-importall.

--- lib/http.rb
+++ lib/http.rb
@@ -1,8 +1,12 @@
+# 20090817
+require 'mechanize_helper'
+
 module HTTP
   
   def get(url, args = {}, agent = 'Windows IE 6', mech_instance = nil)
     uri = URI.parse(url)
-    page = WWW::MechanizeHelper::Page.new(WWW::MechanizeHelper::Url.to_s(uri.scheme, uri.host, uri.port, uri.path, args), {}, agent, mech_instance)
+    url = WWW::MechanizeHelper::Url.to_s(uri.scheme, uri.host, uri.port, uri.path, args)
+    page = WWW::MechanizeHelper::Page.new(url, {}, agent, mech_instance)
     if block_given?
       yield page
     else
@@ -11,15 +15,16 @@ module HTTP
   end
   
   def post(url_or_page_object, args = {}, agent = 'Windows IE 6', mech_instance = nil)
-    page = (
+    url = (
       case url_or_page_object
       when String
         uri = URI.parse(url_or_page_object)
-        WWW::MechanizeHelper::Page.new(WWW::MechanizeHelper::Url.to_s(uri.scheme, uri.host, uri.port, uri.path), args, agent, mech_instance)
+        WWW::MechanizeHelper::Url.to_s(uri.scheme, uri.host, uri.port, uri.path)
       when WWW::MechanizeHelper::Page
-        WWW::MechanizeHelper::Page.new(path_or_page_object.url, args, agent, mech_instance)
+        url_or_page_object.url
       end
     )
+    page = WWW::MechanizeHelper::Page.new(url, args, agent, mech_instance)
     if block_given?
       yield page
     else
