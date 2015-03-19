e2fef67576a9a7d6cb63fed86cc8793de4a4597b
thoran <code@thoran.com>
2015-03-19 19:59:49 +1100

    Committed automatically by git-importall.

--- lib/HTTP/get.rb
+++ lib/HTTP/get.rb
@@ -1,13 +1,16 @@
 # HTTP#get
 
-# 20120610
-# 0.5.0
+# 20120611
+# 0.5.1
 
 # Changes since 0.4: 
 # 0 (Removed Mechanize, since it wasn't working properly, but did 'retain' Nokogiri.)
 # 1. - require 'MechanizeHelper.rbd/MechanizeHelper'.  
 # 2. + require 'Hash/to_parameter_string'.  
 # 3. + require 'nokogiri'.  
+# 0/1 (Added the bare minimum of encoding support.)
+# 4. Added UTF-8 encoding to Nokogiri's parser.  
+# 5. /Nokogiri::HTML/Nokogiri::HTML.parse/.  
 
 require 'Hash/to_parameter_string'
 require 'net/http'
@@ -16,11 +19,11 @@ require 'uri'
 
 module HTTP
   
-  def get(url, params = {})
+  def get(url, params = {}, encoding = 'UTF-8')
     uri = URI.parse(url)
     path = params.empty? ? uri.path : "#{uri.path}?#{params.to_parameter_string}"
     body = Net::HTTP.get(uri.host, path, uri.port)
-    page = Nokogiri::HTML(body)
+    page = Nokogiri::HTML.parse(body, url, encoding)
     if block_given?
       yield page
     else
--- lib/HTTP/post.rb
+++ lib/HTTP/post.rb
@@ -1,13 +1,16 @@
 # HTTP#post
 
-# 20120610
-# 0.5.0
+# 20120611
+# 0.5.1
 
 # Changes since 0.4: 
 # 0 (Removed Mechanize, since it wasn't working properly, but did 'retain' Nokogiri.)
 # 1. - require 'MechanizeHelper.rbd/MechanizeHelper'.  
 # 2. + require 'Hash/to_parameter_string'.  
 # 3. + require 'nokogiri'.  
+# 0/1 (Added the bare minimum of encoding support.)
+# 4. Added UTF-8 encoding to Nokogiri's parser.  
+# 5. /Nokogiri::HTML/Nokogiri::HTML.parse/.  
 
 require 'Hash/to_parameter_string'
 require 'net/http'
@@ -16,9 +19,9 @@ require 'uri'
 
 module HTTP
   
-  def post(url, params = {})
+  def post(url, params = {}, encoding = 'UTF-8')
     body = Net::HTTP.post_form(url, params)
-    page = Nokogiri::HTML(body)
+    page = Nokogiri::HTML.parse(body, url, encoding)
     if block_given?
       yield page
     else
