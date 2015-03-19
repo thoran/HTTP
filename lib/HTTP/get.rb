1425222b069e23319a6743920ea13ff4fe526851
thoran <code@thoran.com>
2015-03-19 19:59:49 +1100

    Committed automatically by git-importall.

--- lib/HTTP/get.rb
+++ lib/HTTP/get.rb
@@ -1,28 +1,48 @@
 # HTTP/get
 # HTTP#get
 
-# 20121002
-# 0.7.0
+# 20130310
+# 0.8.5
 
-# Changes since 0.6: 
-# 1. + module_function :post.  
+# Changes since 0.7: 
+# 1. Changed from using Mechanize and MechanizeHelper to using standard Ruby library objects.  
+# 2. Version number bump to be consistent with HTTP.post.  
 
-require 'MechanizeHelper.rbd/MechanizeHelper'
+require 'net/http'
 require 'uri'
 
-module HTTP
-  
-  def get(url, args = {}, agent = 'Windows IE 6', mech_instance = nil, encoding = 'UTF-8')
-    uri = URI.parse(url)
-    url = MechanizeHelper::Url.to_s(uri.scheme, uri.host, uri.port, uri.path, args)
-    page = MechanizeHelper::Page.new(url, {}, agent, mech_instance, encoding)
-    if block_given?
-      yield page
-    else
-      page
+module URI
+  class HTTP
+
+    def use_ssl?
+      scheme == 'https' ? true : false
     end
+
   end
-  
+end
+
+class Net::HTTP::Get
+
+  def set_headers(headers = {})
+    headers.each{|k,v| self[k] = v}
+  end
+  alias_method :headers=, :set_headers
+
+end
+
+module HTTP
+
+  def get(uri, args = {}, headers = {})
+    uri = URI.parse(uri)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = uri.use_ssl?
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request_object = Net::HTTP::Get.new(uri.request_uri)
+    request_object.headers = headers
+    response = http.request(request_object)
+    response
+  end
+
   module_function :get
-  
+
 end
--- lib/HTTP/post.rb
+++ lib/HTTP/post.rb
@@ -1,8 +1,8 @@
 # HTTP/post.rb
 # HTTP#post.rb
 
-# 20130309
-# 0.8.4
+# 20130310
+# 0.8.5
 
 # Changes: 
 # 1. The parameter, data, can now also accept a :header argument which contains a hash of key-value pairs to be set as part of the header for the request.  
@@ -13,6 +13,8 @@
 # 4. - URI::HTTP.interpolated_port.  
 # 3/4
 # 5. + Net::HTTP::Post#set_headers.  
+# 4/5
+# 6. It now returns the response object and not the body of the response object.  
 
 # Notes: This doesn't return a MechanizeHelper::Page as was intended by the others, but it does work...  (Will get to the MechanizeHelper::Page version later.)  
 
@@ -49,7 +51,7 @@ module HTTP
     request_object.form_data = form_data
     request_object.headers = headers
     response = http.request(request_object)
-    response.body
+    response
   end
 
   module_function :post
